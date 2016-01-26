20e5fc15a3cfa9b9863e16c9f1a7bec37e28a915
Philip Hale <philip@pghale.com>
2016-01-26 14:10:28 +0000

    Fix for Travis; load gem with require_relative

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,15 +1,12 @@
 unless defined?(SPEC_HELPER_LOADED)
   SPEC_HELPER_LOADED = true
 
-  project_root = File.expand_path(File.dirname(__FILE__) + "/..")
   require "rubygems"
   require "bundler/setup"
   require "httparty"
   require "pry"
 
-  accela_lib = "#{project_root}/lib"
-  $LOAD_PATH << accela_lib
-  require "accela"
+  require_relative "../lib/accela"
 
   Accela::Configuration.app_id = "635395466279594650"
   Accela::Configuration.app_secret = "3b1e4026d95e4478a0f8dd1f7a1b7028"
