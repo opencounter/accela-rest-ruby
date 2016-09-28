c17aed122f654172feb7f95dae7adba8ac30f751
Philip Hale <philip@pghale.com>
2016-09-28 12:04:50 +0100

    PUT instead of POST to update record contact forms+tables

--- lib/accela/api/v4/update_record_contact_custom_forms.rb
+++ lib/accela/api/v4/update_record_contact_custom_forms.rb
@@ -2,7 +2,7 @@ module Accela
   module V4
     class UpdateRecordContactCustomForms < Base
       def call(record_id, contact_id, payload)
-        post("records/#{record_id}/contacts/#{contact_id}/customForms", :access_token, {}, payload)
+        put("records/#{record_id}/contacts/#{contact_id}/customForms", :access_token, {}, payload)
       end
     end
   end
